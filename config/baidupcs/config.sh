4c909636a7f4cbcbdd71c49626d371267ddb17a6
YadominJinta <1782397074@qq.com>
2017-07-17 18:50:35 +0800

    完善baidupcs

--- config/baidupcs/config.sh
+++ config/baidupcs/config.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+echo 安装依赖
+apt update
+apt install -y curl-dev openssl-dev openssl
 echo 下载源码
 git clone https://github.com/GangZhuo/BaiduPCS
 echo 安装依赖
@@ -9,4 +12,6 @@ cc=clang
 ./configure
 make
 mv baidupcs $PREFIX/bin
+cd ..
+rm -rf baidupcs
 echo 安装结束，使用愉快
